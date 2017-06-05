c8f6ec34e2e8b05689bfde046ff0c578b522f98d
jzander <jeremy.zander@gmail.com>
2017-06-05 14:16:34 -0700

    update angular

--- app.js
+++ app.js
@@ -7,27 +7,79 @@ angular.module('brandonDermer', [])
   .controller('brandonController', ["$scope", "PROJECTS",
     function ($scope, PROJECTS) {
       console.log(PROJECTS, "are the projects");
+
+      $scope.musicVideos = PROJECTS.musicVideos;
+      $scope.commercials = PROJECTS.commercials;
+      $scope.shorts = PROJECTS.shorts;
+
+      console.log("music videos: ", $scope.musicVideos, "commercials: ",  $scope.commercials, "shorts: ", $scope.shorts);
     }
   ]).constant("PROJECTS", {
-  "projects": [
+  "musicVideos": [
     {
       artist: "Panic At The Disco",
       title: "Victorious",
-      link: "https://youtu.be/AUChk0lxF44",
+      href: "https://youtu.be/AUChk0lxF44",
       role: "Director/Writer",
       img: "panic at the disco.png"
     },
     {
       artist: "Panic At The Disco",
       title: "Victorious",
-      link: "https://youtu.be/AUChk0lxF44",
+      href: "https://youtu.be/AUChk0lxF44",
       role: "Director/Writer",
       img: "panic at the disco.png"
     },
     {
       artist: "Panic At The Disco",
       title: "Victorious",
-      link: "https://youtu.be/AUChk0lxF44",
+      href: "https://youtu.be/AUChk0lxF44",
+      role: "Director/Writer",
+      img: "panic at the disco.png"
+    }
+  ],
+  "shorts": [
+    {
+      artist: "Panic At The Disco",
+      title: "Victorious",
+      href: "https://youtu.be/AUChk0lxF44",
+      role: "Director/Writer",
+      img: "panic at the disco.png"
+    },
+    {
+      artist: "Panic At The Disco",
+      title: "Victorious",
+      href: "https://youtu.be/AUChk0lxF44",
+      role: "Director/Writer",
+      img: "panic at the disco.png"
+    },
+    {
+      artist: "Panic At The Disco",
+      title: "Victorious",
+      href: "https://youtu.be/AUChk0lxF44",
+      role: "Director/Writer",
+      img: "panic at the disco.png"
+    }
+  ],
+  "commercials": [
+    {
+      artist: "Panic At The Disco",
+      title: "Victorious",
+      href: "https://youtu.be/AUChk0lxF44",
+      role: "Director/Writer",
+      img: "panic at the disco.png"
+    },
+    {
+      artist: "Panic At The Disco",
+      title: "Victorious",
+      href: "https://youtu.be/AUChk0lxF44",
+      role: "Director/Writer",
+      img: "panic at the disco.png"
+    },
+    {
+      artist: "Panic At The Disco",
+      title: "Victorious",
+      href: "https://youtu.be/AUChk0lxF44",
       role: "Director/Writer",
       img: "panic at the disco.png"
     }
